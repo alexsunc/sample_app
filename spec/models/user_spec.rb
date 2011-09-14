40eaae73ec96c43f51249d2d1d580645f220ca99
kift <su.shihjie@gmail.com>
2011-09-14 15:58:09 +0800

    Added password and encrypted_password attributes to the User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,12 @@ require 'spec_helper'
 describe User do
   
   before(:each) do
-    @attr = { :name => "Example User", :email => "user@example.com" }
+    @attr = { 
+      :name => "Example User",
+      :email => "user@example.com",
+      :password => "foobar",
+      :password_confirmation => "foobar" 
+    }
   end
   
   it "should create a new instance given a valid attribute" do
@@ -58,16 +63,70 @@ describe User do
     user_with_duplicate_email.should_not be_valid
   end
   
+  describe "password" do
+    
+    before(:each) do
+      @user = User.new(@attr)
+    end
+    
+    it "should have a password attribute" do
+      # User.new(@attr).should respond_to(:password)
+      @user.should respond_to(:password)
+    end
+    
+    it "should have a password confirmation attribute" do
+      @user.should respond_to(:password_confirmation)
+    end    
+  end
+  
+  describe "password validations" do
+    
+    it "should require a password" do
+      User.new(@attr.merge(:password => "", :password_confirmation => "")).
+          should_not be_valid
+    end
+    
+    it "should require a matching password confirmation" do
+      User.new(@attr.merge(:password_confirmation => "invalid")).
+          should_not be_valid
+    end
+    
+    it "should reject short passowrds" do
+      short = "a" * 5
+      hash = @attr.merge(:passowrd => short, :password_confirmation => short)
+      User.new(hash).should_not be_valid
+    end
+    
+    it "should reject long passowrds" do
+      long = "a" * 41
+      hash = @attr.merge(:passowrd => long, :password_confirmation => long)
+      User.new(hash).should_not be_valid
+    end
+  end
+  
+  describe "password encryption" do
+    
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+    
+    it "should have an encrypted password attribute" do
+      @user.should respond_to(:encrypted_password)
+    end    
+  end
+    
 end
+
 
 # == Schema Information
 #
 # Table name: users
 #
-#  id         :integer(4)      not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer(4)      not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
 #
 
