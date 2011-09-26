6f6c174f500e61d6055f539556db9f2d6a742e78
kift <su.shihjie@gmail.com>
2011-09-27 00:28:13 +0800

    Initial interface for following/followers

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 SampleApp::Application.routes.draw do
   
-  resources :sessions,   :only => [:new, :create, :destroy]
-  resources :microposts, :only => [:create, :destroy]
+  resources :sessions,      :only => [:new, :create, :destroy]
+  resources :microposts,    :only => [:create, :destroy]
+  resources :relationships, :only => [:create, :destroy]
 
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   # get "users/new"
 
   root :to => "pages#home"
